c453d9fb3535342073890d24c6e149f3775ff9b4
Thomas Paul <tpaul19@wgu.edu>
2021-05-28 05:34:22 -0500

    Init commit

---
+++ pylogger.py
@@ -0,0 +1,12 @@
+class Logger():
+    
+    # Constants used to pass in event types
+    ERROR = "Error"
+    DATA_SCRAPE = "Data Scrape"
+    DATA_PERSIST = "Data Inserted Into DB"
+
+    def __init__(self, logfile_dst):
+        pass
+
+    def log(self, event_type, message):
+        pass
