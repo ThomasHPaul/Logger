9c5738ff0d228ea01a14fcc618a45955093e2823
Thomas Paul <tpaul19@wgu.edu>
2021-05-28 05:47:57 -0500

    Added constructor and log methods

--- pylogger.py
+++ pylogger.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Logger():
     
     # Constants used to pass in event types
@@ -5,8 +7,14 @@ class Logger():
     DATA_SCRAPE = "Data Scrape"
     DATA_PERSIST = "Data Inserted Into DB"
 
-    def __init__(self, logfile_dst):
-        pass
+    def __init__(self, app_name, logfile_dst=None):
+        if logfile_dst is None:
+            logfile = app_name + "_log.txt"
+        else:
+            logfile = logfile_dst + app_name + "_log.txt"
+
 
     def log(self, event_type, message):
-        pass
+        log_string = str(datetime.now()) + "\t" + event_type + "\t" + message
+        with open(self.logfile, 'a+') as f:
+            f.write(log_string)
